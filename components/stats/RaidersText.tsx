af7555f1987254f7effbdba6f9b4814593e414a2
Compositr <43405050+Compositr@users.noreply.github.com>
2023-06-22 13:58:50 +1000

    feat: cache raiders count

--- components/stats/RaidersText.tsx
+++ components/stats/RaidersText.tsx
@@ -1,7 +1,11 @@
+import { cache } from "react"
+
 import prisma from "@/lib/prisma"
 
+const cachedCount = cache(async () => prisma.user.count())
+
 export default async function RaidersText() {
-  const count = await prisma.user.count()
+  const count = await cachedCount()
 
   return <span>{count.toLocaleString()}</span>
 }
