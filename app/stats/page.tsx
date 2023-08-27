db25c89e05a8857fbb26fa3fed5d7f29877f8861
Compositr <compositr@compositr.dev>
2023-08-27 22:52:23 +1000

    feat: use new statsection component, pomelo stats

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,9 +1,14 @@
+import { Metadata } from "next"
+
 import BlacklisterText from "@/components/stats/BlacklisterText"
+import PomeloPercentText from "@/components/stats/PomeloPercentText"
 import RaidersText from "@/components/stats/RaidersText"
 import RaidsText from "@/components/stats/RaidsText"
 import ServersText from "@/components/stats/ServersText"
 import StatCard from "@/components/stats/StatCard"
-import { Metadata } from "next"
+import StatSection from "@/components/stats/StatSection"
+import PomeloCountText from "@/components/stats/PomeloCountText"
+import PomeloRecentPercentText from "@/components/stats/PomeloRecentPercentText"
 
 export const metadata: Metadata = {
   title: "Statistics",
@@ -13,9 +18,9 @@ export const metadata: Metadata = {
 export default async function StatsPage() {
   return (
     <div>
-      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <section className="container grid gap-6 items-center pt-6 pb-8 md:py-10">
         <div className="flex max-w-[980px] flex-col items-start gap-2">
-          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          <h1 className="text-3xl font-extrabold tracking-tighter leading-tight md:text-4xl">
             Statistics
           </h1>
           <p className="max-w-[700px] text-lg text-muted-foreground">
@@ -23,7 +28,7 @@ export default async function StatsPage() {
           </p>
         </div>
       </section>
-      <section className="container flex flex-col flex-wrap gap-4 md:flex-row">
+      <StatSection>
         <StatCard title="Total Users" footer="...and counting!">
           <RaidersText />
         </StatCard>
@@ -33,15 +38,39 @@ export default async function StatsPage() {
         >
           <BlacklisterText />
         </StatCard>
-      </section>
-      <section className="container mt-4 flex flex-col flex-wrap gap-4 md:flex-row">
+      </StatSection>
+      <StatSection>
         <StatCard title="Total Raids">
           <RaidsText />
         </StatCard>
         <StatCard title="Total Servers">
           <ServersText />
         </StatCard>
+      </StatSection>
+      <section className="container grid gap-6 items-center pt-6 pb-8 md:py-10">
+        <div className="flex max-w-[980px] flex-col items-start gap-2">
+          <h2 className="text-2xl font-extrabold tracking-tighter leading-tight md:text-3xl">
+            Pomelo Stats
+          </h2>
+          <p className="max-w-[700px] text-md text-muted-foreground">
+            Tracking Discord's Pomelo uptake by raiders and userbots
+          </p>
+        </div>
       </section>
+      <StatSection>
+        <StatCard
+          title="Pomelo Percentage"
+          footer="Percentage of users with a new Pomelo username (usernames with no # numbers)"
+        >
+          <PomeloPercentText />
+        </StatCard>
+        <StatCard title="Pomelo Users">
+          <PomeloCountText />
+        </StatCard>
+        <StatCard title="Recent Pomelo Percentage" footer="Percentage of Pomelo users in the past 30d">
+          <PomeloRecentPercentText />
+        </StatCard>
+      </StatSection>
     </div>
   )
 }
