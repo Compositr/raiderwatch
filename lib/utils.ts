3c39c9becddcbf2e7c0be7f94f055cd75b0267fb
Compositr <compositr@compositr.dev>
2023-09-12 21:43:10 +1000

    feat: add new util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function truncateDateToDay(date: Date): Date {
+  date.setHours(0, 0, 0, 0)
+  return date
 }
