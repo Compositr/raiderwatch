cdd9884529393f502311030afa648d62e4a619ba
Compositr <compositr@compositr.dev>
2023-09-16 11:12:33 +1000

    feat: cache blacklister * remove uneeded auth

--- components/stats/BlacklisterText.tsx
+++ components/stats/BlacklisterText.tsx
@@ -1,11 +1,17 @@
-export default async function BlacklisterText() {
-  const stats = await fetch("https://api.blacklister.xyz/stats", {
-    headers: {
-      Authorization: process.env.BLACKLISTER!,
-    },
+import { cache } from "react"
+
+export const revalidate = 10
+
+export const blacklister = cache(() =>
+  fetch("https://api.blacklister.xyz/stats", {
     next: {
       revalidate: 60,
     },
   }).then((res) => res.json())
+)
+
+export default async function BlacklisterText() {
+  const stats = await blacklister()
+
   return <span>{stats.users.toLocaleString()}</span>
 }
