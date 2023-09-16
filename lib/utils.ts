19837b62bc561a6e1ff2649e5b7fd583d23eaa1d
Compositr <compositr@compositr.dev>
2023-09-16 11:03:57 +1000

    feat: add truncated1h function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,8 +11,15 @@ export function truncateDateToDay(date: Date): Date {
 }
 
 export function truncated30d() {
-  const date = new Date();
-  date.setHours(0, 0, 0, 0); // Set time to the start of the current day
-  date.setDate(date.getDate() - 30); // Subtract 30 days
-  return date;
+  const date = new Date()
+  date.setHours(0, 0, 0, 0) // Set time to the start of the current day
+  date.setDate(date.getDate() - 30) // Subtract 30 days
+  return date
+}
+
+export function truncated1h() {
+  const date = new Date()
+  date.setHours(date.getHours(), 0, 0, 0) // Set time to the start of the hour
+  date.setDate(date.getHours() - 1) // Subtract 1 hour
+  return date
 }
